ea513511dafc85df54f59843b6a63f25d6bb92a8
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2026-06-17 13:05:25 +0330

    Basic API

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&q-ose%or(cp(b88t$!t^1aifm$xe4=vf1c!(21-d1hx8il!+3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
