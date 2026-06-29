d9a18a79f91006899b3fa4316373208822a5ddb1
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2026-06-29 13:12:10 +0330

    .....

--- config/middleware.py
+++ config/middleware.py
@@ -3,9 +3,18 @@ class APICorsMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
+        # OPTIONS request رو مستقیم جواب بده بدون authentication
+        if request.method == "OPTIONS":
+            from django.http import HttpResponse
+            response = HttpResponse()
+            response["Access-Control-Allow-Origin"] = "*"
+            response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+            response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
+            response["Access-Control-Max-Age"] = "86400"
+            return response
+
         response = self.get_response(request)
         response["Access-Control-Allow-Origin"] = "*"
         response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
         response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
         return response
-    
