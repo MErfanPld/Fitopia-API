fa2b538844fd4f7a2d2cd3b5dfe58cc5c383d5a7
MErfanPld <m.erfanpouladzadeh@yahoo.com>
2026-08-04 11:12:44 +0330

    ......

--- config/middleware.py
+++ config/middleware.py
@@ -8,13 +8,13 @@ class APICorsMiddleware:
             from django.http import HttpResponse
             response = HttpResponse()
             response["Access-Control-Allow-Origin"] = "*"
-            response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+            response["Access-Control-Allow-Methods"] = "GET, POST, PUT,PATCH, DELETE, OPTIONS"
             response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
             response["Access-Control-Max-Age"] = "86400"
             return response
 
         response = self.get_response(request)
         response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT,PATCH, DELETE, OPTIONS"
         response["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
         return response
