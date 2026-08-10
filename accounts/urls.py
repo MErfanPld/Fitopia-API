8989d16840a80ac9566eb3d4082d44b7a65b6e6b
Mohammad Erfan Pouladzadeh <86192924+MErfanPld@users.noreply.github.com>
2026-08-10 12:08:13 +0330

    fix: stabilize auth, tokens, subscriptions and harden configuration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import RegisterView, LoginView,LogoutView, UserProfileAPIView
+from .views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    UserProfileAPIView,
+    ChangePasswordView,
+)
 
 urlpatterns = [
     path("register/", RegisterView.as_view()),
     path("login/", LoginView.as_view()),
     path("logout/", LogoutView.as_view()),
-    path(
-        "profile/",
-        UserProfileAPIView.as_view(),
-        name="user-profile",
-    ),
+    path("profile/", UserProfileAPIView.as_view(), name="user-profile"),
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
 ]
-
