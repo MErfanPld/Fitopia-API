a06515d0c92fd0b42bd3c31362221ffe27607ca6
Mohammad Erfan Pouladzadeh <86192924+MErfanPld@users.noreply.github.com>
2026-08-10 12:09:46 +0330

    fix(config): env-based settings and media URL serving in DEBUG

--- config/urls.py
+++ config/urls.py
@@ -1,33 +1,18 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-    path('api/gym/', include('gym.urls')),
-    path('api/core/', include('core.urls')),
-    path('api/subscriptions/', include('subscriptions.urls')),
+    path("admin/", admin.site.urls),
+    path("api/accounts/", include("accounts.urls")),
+    path("api/gym/", include("gym.urls")),
+    path("api/core/", include("core.urls")),
+    path("api/subscriptions/", include("subscriptions.urls")),
     path("api/gym-panel/", include("gym_panel.urls")),
     path("api/tokens/", include("tokens.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
@@ -36,4 +21,7 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
-    ]
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
